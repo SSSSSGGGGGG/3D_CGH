5090c85c75595694b9e5482cd49dcc2d7267900d
SSSSSGGGGGG <sgao@umh.es>
2025-04-10 17:02:05 +0200

    4.10

--- 3d_FFT/3d_FFT.py
+++ 3d_FFT/3d_FFT.py
@@ -7,7 +7,7 @@ Created on Thu Apr  3 15:05:31 2025
 
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.fft import fftn, ifftn
+from scipy.fft import fftn, ifftn, fftshift
 from mpl_toolkits.mplot3d import Axes3D
 
 # Step 1: Create or load 3D data cube
@@ -43,34 +43,63 @@ ax.set_ylim(0, size)
 ax.set_zlim(0, size)
 plt.show()
 
+# fig2=plt.figure()
+# # Subplot 2: sparse full cube
+# ax2 = fig2.add_subplot(111, projection="3d")
+# sc2 = ax2.scatter(x2[mask_bl], y2[mask_bl], z2[mask_bl], c=cube_bl[mask_bl], cmap="hot", s=5)
+# ax2.set_title("Filled Cube (Sparse)")
+# plt.colorbar(sc2, ax=ax2)
+# ax2.set_xlim(0, size_s)
+# ax2.set_ylim(0, size_s)
+# ax2.set_zlim(0, size_s)
+# plt.show()
+
+# cube = np.random.rand(size, size, size)
+rand=np.random.uniform(size, size, size)
+rand_2pi=rand*2*np.pi
+# cube=cube*rand_2pi
+# # Step 2: FFT
+# fft_cube = fftn(cube_em)
+field=np.sqrt(cube_em)*rand_2pi
+fft_cube = fftshift(fftn(field))
+max_=abs(fft_cube).max()
+mask_fft=fft_cube>0.05*max_
 fig2=plt.figure()
 # Subplot 2: sparse full cube
 ax2 = fig2.add_subplot(111, projection="3d")
-sc2 = ax2.scatter(x2[mask_bl], y2[mask_bl], z2[mask_bl], c=cube_bl[mask_bl], cmap="hot", s=5)
-ax2.set_title("Filled Cube (Sparse)")
+sc2 = ax2.scatter(x[mask_fft], y[mask_fft], z[mask_fft], c=abs(fft_cube)[mask_fft], cmap="hot", s=5)
+ax2.set_title("FFT Cube")
 plt.colorbar(sc2, ax=ax2)
-ax2.set_xlim(0, size_s)
-ax2.set_ylim(0, size_s)
-ax2.set_zlim(0, size_s)
-
+ax2.set_xlim(0, size)
+ax2.set_ylim(0, size)
+ax2.set_zlim(0, size)
 plt.show()
-# cube = np.random.rand(size, size, size)
-# rand=np.random.rand(size, size, size)
-# rand_2pi=rand*2*np.pi
-# cube=cube*rand_2pi
-# # Step 2: FFT
-# fft_cube = fftn(cube)
 
-# # Step 3: Extract phase only
-# phase = np.angle(fft_cube)
-# phase_only_cube = np.exp(1j * phase)
+# plt.figure()
+# plt.imshow(abs(fft_cube)[:,:,100],cmap="hot")
+# plt.show()
 
-# # Step 4: Inverse FFT using phase only
-# reconstructed = ifftn(phase_only_cube)
+# Step 3: Extract phase only
+phase = np.angle(fft_cube)
+phase_only_cube = np.exp(1j * phase)
 
-# # Step 5: Intensity (squared magnitude)
-# intensity = np.abs(reconstructed)#**2
+# Step 4: Inverse FFT using phase only
+reconstructed = ifftn(abs(fft_cube)*phase_only_cube)
 
+# Step 5: Intensity (squared magnitude)
+intensity = np.abs(reconstructed)#**2
+h,w,t=intensity.shape
+x3, y3, z3 = np.indices((h,w,t))
+re_mask=intensity>0.0005*max_
+fig3=plt.figure()
+ax3 = fig3.add_subplot(111, projection="3d")
+sc3 = ax3.scatter(x3[re_mask], y3[re_mask], z3[re_mask], c=intensity[re_mask], cmap="hot", s=5)
+ax2.set_title("FFT Cube")
+plt.colorbar(sc3, ax=ax3)
+ax2.set_xlim(0, size)
+ax2.set_ylim(0, size)
+ax2.set_zlim(0, size)
+plt.show()
 # # Step 6: Threshold for visualization
 # threshold = 0.7 * intensity.max()
 # coords = np.array(np.where(intensity > threshold)).T
